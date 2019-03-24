31335dfe63911602748f5210e15df9f6f340d76a
Omri Spector <omri.spector@gmail.com>
2019-03-24 17:28:14 +0200

    adds well
    Resolved conflicts at multiply branch

--- calculator.py
+++ calculator.py
@@ -33,8 +33,8 @@ if choice == '1':
 elif choice == '2':
    print(num1,"-",num2,"=",subtract(num1,num2))
 elif choice == '3':
-   print("Not implemented yet")
+   print(x,"*",y,"=", multiply(x,y))
 elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
 else:
-   print("Invalid input ",choice)
+   print("Invalid input ", choice)
