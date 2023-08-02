92010e4c0117126b01a7830451525ccd5cec4654
Harel Sultan <harel714@gmail.com>
2023-08-02 20:55:14 +0300

    Fixed variable naming..

--- calculator.py
+++ calculator.py
@@ -33,7 +33,7 @@ if choice == '1':
 elif choice == '2':
    print(num1,"-",num2,"=",subtract(num1,num2))
 elif choice == '3':
-   print(x,"*",y,"=", multiply(x,y))
+   print(num1,"*",num2,"=", multiply(num1,num2))
 elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
 else:
